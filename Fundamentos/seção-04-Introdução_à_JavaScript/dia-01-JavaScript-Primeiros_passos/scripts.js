f9fd955e468b367ced187792fa3cee49d2c25531
leonardo caixeta <leonardocaixeta@gmail.com>
2023-02-28 02:39:14 -0300

    Ponto 2.4

--- Fundamentos/seção-04-Introdução_à_JavaScript/dia-01-JavaScript-Primeiros_passos/scripts.js
+++ Fundamentos/seção-04-Introdução_à_JavaScript/dia-01-JavaScript-Primeiros_passos/scripts.js
@@ -1,11 +1,34 @@
-let penteaderaCusto = 300
-let penteaderaValor = 600
-let lucro = penteaderaValor * 1000 - penteaderaCusto * 1000
-let imposto = penteaderaCusto * (20/100)
-let lucroTotal = lucro - lucroImposto
+let salario = 3750;
+let inss = "";
+let receitaFederal = "";
 
-if (penteaderaCusto <= 0 || penteaderaValor <= 0){
-  console.log("ERRO")
+if (salario < 1556.94) {
+    inss = salario * (8 / 100);
+    console.log('você deve ' + inss + ' de inss');
+} else if (salario > 1556.94 && salario < 2594.92) {
+    inss = salario * (9 / 100);
+    console.log('você deve ' + inss + ' de inss');
+} else if (salario > 2594.93 && salario < 5189.82) {
+    inss = salario * (11 / 100);
+    console.log('você deve ' + inss + ' de inss');
 } else {
- console.log("Tudo certo")
+    console.log('você deve 570.88 de inss');
 }
+if (salario < 1903.98) {
+    console.log("O leão da receita não te pegou");
+} else if (salario > 1903.98 && salario < 2826.65) {
+    receitaFederal = salario * (7.5 / 1000) + 142.80
+    console.log("voce deve " + receitaFederal + " de imposto de renda, com uma parcela de 142.80 a deduzir ")
+} else if (salario > 2826.66 && salario < 3751.05) {
+    receitaFederal = salario * (15 / 1000) + 354.80
+    console.log("voce deve " + receitaFederal + " de imposto de renda, com uma parcela de 354.80 a deduzir")
+} else if (salario > 3751.06 && salario < 4664.68) {
+    receitaFederal = salario * (22.5 / 1000) + 636.13
+    console.log("voce deve " + receitaFederal + " de imposto de renda, com uma parcela de 636,13 a deduzir")
+} else {
+    receitaFederal = salario * (27.5 / 1000) + 869.36
+    console.log("voce deve " + receitaFederal + " de imposto de renda, com uma parcela de 869.36 a deduzir")
+}
+
+let salarioFinal = salario - inss - receitaFederal
+console.log('Restou ' + salarioFinal);
