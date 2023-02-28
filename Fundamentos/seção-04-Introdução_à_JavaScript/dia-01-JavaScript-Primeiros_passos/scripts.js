4c631a99f212de2d7c570838902c5f3e755dbf4a
leonardo caixeta <leonardocaixeta@gmail.com>
2023-02-27 23:00:11 -0300

    Ponto 1.5

--- Fundamentos/seção-04-Introdução_à_JavaScript/dia-01-JavaScript-Primeiros_passos/scripts.js
+++ Fundamentos/seção-04-Introdução_à_JavaScript/dia-01-JavaScript-Primeiros_passos/scripts.js
@@ -1,11 +1,25 @@
-let num1 = 50;
-let num2 = 70;
-let num3 = 50;
+let peça = "kNight";
 
-if (num1 + num2 + num3 == 180) {
-  console.log("True");
-}
-  else{
-  console.log("False");
-  console.log("Falta de ângulos");
+switch (peça.toLowerCase()) {
+  case "knight":
+    console.log("se move em L (3 para frente e um para o lado)");
+break;
+  case"queen":
+    console.log("se move para todas as direções");
+break;
+  case "king":
+  console.log("se move um quadrado por direção");
+break;
+  case "bishop":
+    console.log("se move nas diagonais");
+break;
+  case "hook":
+    console.log("se move para frente e para os lados ");
+break;
+  case "pawn":
+    console.log("no primeiro movimento pode-se pular 2 quadrados para frente, após,somente 1 quadrado por vez, pegando peças pulando na diagonal");
+break;
+  default:
+    console.log("Peça inexistente");
+break;
 }
