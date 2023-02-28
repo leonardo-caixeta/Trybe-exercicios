da0a67b5618c6c0c29b9d5e83148308d32c19b87
leonardo caixeta <leonardocaixeta@gmail.com>
2023-02-27 22:12:01 -0300

    Ponto 1.3

---
+++ Fundamentos/seção-04-Introdução_à_JavaScript/dia-01-JavaScript-Primeiros_passos/scripts.js
@@ -0,0 +1,13 @@
+let num1 = 3;
+let num2 = -7;
+let num3 = 0;
+let res = num1 + num2;
+
+if (num2 > num1 && num2 > num3){
+    console.log("num2 ganhou")
+}
+  else if (num1 > num2 && num1 >num3){
+    console.log("num1 ganhou");
+}
+  else if (num3 > num1 && num3 > num1)
+    console.log("num3 ganhou");
