5e4fb919e7afcc8ea532cb3afdb5eea280064bf7
leonardo caixeta <leonardocaixeta@gmail.com>
2023-02-28 00:33:44 -0300

    Ponto 2.1

--- Fundamentos/seção-04-Introdução_à_JavaScript/dia-01-JavaScript-Primeiros_passos/scripts.js
+++ Fundamentos/seção-04-Introdução_à_JavaScript/dia-01-JavaScript-Primeiros_passos/scripts.js
@@ -1,26 +1,9 @@
-let percent = 72;
+let a = 2
+let b = 3
+let c = 5
 
-if (percent < 100) {
-  console.log("ERRO_Maior que 100");
+if (a % 2 == 0 || b % 2 == 0 || c % 2 ==0) {
+  console.log("É par essa cascaveli");
+} else {
+  console.log("Ìmpa")
 }
-  else if (percent < 0){
-    console.log("ERRO_Menor que 0");
-  }
-  else if (percent >= 90) {
-    console.log("A");
-  }
-  else if (percent >= 80 && percent < 90) {
-    console.log("B");
-  }
-  else if (percent >= 70 && percent < 80) {
-    console.log("C");
-  }
-  else if (percent >= 60 && percent < 70) {
-    console.log("D");
-  }
-  else if (percent >= 50 && percent < 60) {
-    console.log("E")
-  }
-  else if (percent < 50 && percent > 0) {
-    console.log("F");
-  }
