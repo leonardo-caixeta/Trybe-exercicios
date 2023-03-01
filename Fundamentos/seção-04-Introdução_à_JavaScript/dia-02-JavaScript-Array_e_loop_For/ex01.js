232b4e3930413267445a7c50719543a7bba46091
leonardo caixeta <leonardocaixeta@gmail.com>
2023-02-28 23:53:10 -0300

    Ponto 1.5

--- Fundamentos/seção-04-Introdução_à_JavaScript/dia-02-JavaScript-Array_e_loop_For/ex01.js
+++ Fundamentos/seção-04-Introdução_à_JavaScript/dia-02-JavaScript-Array_e_loop_For/ex01.js
@@ -4,10 +4,30 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 //   console.log(numbers[index])
 // }
 
-let result = 0 ;
+// ----------------------------------------------------
 
-for (let index = 0; index < numbers.length; index += 1) {
-  result += numbers[index]
-  break;
+// let result = 0 ;
+// let avarage = 0
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   result += numbers[index];
+//   avarage = result / index;
+//   }
+//   if(avarage > 20){
+//     console.log('Valor maior que 20')
+//   } else {
+//     console.log('Valor menor que 20');
+//   }
+// console.log('resultado ' + result + ' média ' +  avarage )
+
+// ----------------------------------------------------
+
+let higherNumber = numbers[0]
+
+for(index = 1; index < numbers.length; index += 1){
+  if (numbers[index] > higherNumber){
+    higherNumber = numbers[index]
+  }
 }
-console.log(result)
+
+console.log(higherNumber);
