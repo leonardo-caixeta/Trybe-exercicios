14e3dfbfc196b5b609fbb58c92556c8175e6520c
leonardo caixeta <leonardocaixeta@gmail.com>
2023-03-01 00:39:56 -0300

    Ponto 1.6

--- Fundamentos/seção-04-Introdução_à_JavaScript/dia-02-JavaScript-Array_e_loop_For/ex01.js
+++ Fundamentos/seção-04-Introdução_à_JavaScript/dia-02-JavaScript-Array_e_loop_For/ex01.js
@@ -7,7 +7,7 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 // ----------------------------------------------------
 
 // let result = 0 ;
-// let avarage = 0
+// let avarage = 0 ;
 
 // for (let index = 0; index < numbers.length; index += 1) {
 //   result += numbers[index];
@@ -22,12 +22,43 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // ----------------------------------------------------
 
-let higherNumber = numbers[0]
+// let higherNumber = numbers[0] ;
 
-for(index = 1; index < numbers.length; index += 1){
-  if (numbers[index] > higherNumber){
-    higherNumber = numbers[index]
+// for(index = 1; index < numbers.length; index += 1){
+//   if (numbers[index] > higherNumber){
+//     higherNumber = numbers[index]
+//   }
+// }
+
+// console.log(higherNumber);
+
+// ----------------------------------------------------
+
+// let oddNumbers = [0] ;
+
+// for (let index = 0; index < numbers.length; index += 1) {
+//   if (numbers[index] % 2 != 0 ) {
+//     oddNumbers.push(numbers[index]) ;
+//   } else if (oddNumbers === 0) {
+//      console.log('Nenhum numero ímpar encontrado');
+//   } {
+
+//   }
+// }
+// console.log(oddNumbers);
+
+// ----------------------------------------------------
+
+let smallestNumber = numbers[0] ;
+
+for (let index = 0; index < numbers.length; index += 1) {
+  // let currentNumber = numbers[index];
+  if (smallestNumber < numbers[index]) {
+    smallestNumber = numbers[index];
+  }else{
+
   }
+
 }
 
-console.log(higherNumber);
+console.log(smallestNumber)
