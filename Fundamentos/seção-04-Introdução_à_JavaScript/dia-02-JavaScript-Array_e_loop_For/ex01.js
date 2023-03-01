db0ac4013324baa38538047f7d92ed5bcb29d77f
leonardo caixeta <leonardocaixeta@gmail.com>
2023-03-01 01:06:27 -0300

    Ponto 1.8

--- Fundamentos/seção-04-Introdução_à_JavaScript/dia-02-JavaScript-Array_e_loop_For/ex01.js
+++ Fundamentos/seção-04-Introdução_à_JavaScript/dia-02-JavaScript-Array_e_loop_For/ex01.js
@@ -49,16 +49,27 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 // ----------------------------------------------------
 
-let smallestNumber = numbers[0] ;
+// let smallestNumber = numbers[0] ;
 
-for (let index = 0; index < numbers.length; index += 1) {
-  // let currentNumber = numbers[index];
-  if (smallestNumber < numbers[index]) {
-    smallestNumber = numbers[index];
-  }else{
+// for (let index = 0; index < numbers.length; index += 1) {
+//   let currentNumber = numbers[index];
+//   if (smallestNumber > currentNumber) {
+//     smallestNumber = currentNumber;
+//   }else{
 
-  }
+//   }
 
+// }
+
+// console.log(smallestNumber)
+
+// ----------------------------------------------------
+
+let list = [];
+
+for (let index = 1; index < 26; index += 1) {
+  list.push(index)
 }
 
-console.log(smallestNumber)
+console.log(list);
+// -----------------------------------------------------
