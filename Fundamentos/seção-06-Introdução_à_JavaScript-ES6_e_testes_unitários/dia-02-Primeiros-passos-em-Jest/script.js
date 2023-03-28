a7baa391f297a60c80ecd44bcf6f2e0a96756ac9
leonardo caixeta <leonardocaixeta@gmail.com>
2023-03-28 00:58:28 -0300

    ex 4

--- Fundamentos/seção-06-Introdução_à_JavaScript-ES6_e_testes_unitários/dia-02-Primeiros-passos-em-Jest/script.js
+++ Fundamentos/seção-06-Introdução_à_JavaScript-ES6_e_testes_unitários/dia-02-Primeiros-passos-em-Jest/script.js
@@ -52,9 +52,25 @@ const decode = (string) => {
   return mapString(map, string);
 }
 
+const techList = (arrayTechnologies, name) => {
+  if (arrayTechnologies.length === 0) return 'Vazio!';
+
+  const sortedArray = arrayTechnologies.sort();
+  const technologyList =  [];
+
+  for (let index = 0; index < sortedArray.length; index += 1) {
+    technologyList.push({
+      tech: sortedArray[index],
+      name,
+    });
+  }
+  return technologyList;
+};
+
 module.exports = {
   myRemove,
   myFizzBuzz,
   encode,
-  decode
+  decode,
+  techList
 };
